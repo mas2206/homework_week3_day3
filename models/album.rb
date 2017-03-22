aa313023d8493296c66174651ed6fac3c7d5b52a
Michael Shennan <m.shennan22@gmail.com>
2017-03-22 15:40:41 +0000

    set up artist and album classes

---
+++ models/album.rb
@@ -0,0 +1,10 @@
+class Album
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
+  end
+
+end
